93f83833a8f82f2e68fcfb3a7f66e8a5d6aa9eca
Vincent <vincentlocnguyen@gmail.com>
2025-06-08 11:50:30 -0700

    force updates in production when they're available

--- src/update-manager.tsx
+++ src/update-manager.tsx
@@ -1,44 +1,16 @@
 import * as OfflinePluginRuntime from "@lcdp/offline-plugin/runtime";
 import { useEffect } from "react";
-import { toaster } from "./toaster";
-import { Intent } from "@blueprintjs/core";
-import { useIntl } from "./hooks/useIntl";
-import { Outdated } from "@blueprintjs/icons";
 
 export function UpdateManager() {
-  const { t } = useIntl();
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
       OfflinePluginRuntime.install({
         onUpdateReady() {
           OfflinePluginRuntime.applyUpdate();
-          toaster.show(
-            {
-              message: t("updateLoading"),
-              intent: Intent.WARNING,
-            },
-            "UpdateManager",
-          );
-        },
-        onUpdated() {
-          toaster.clear();
-          toaster.show(
-            {
-              message: t("updateReady"),
-              icon: <Outdated />,
-              intent: Intent.SUCCESS,
-              timeout: 0,
-              action: {
-                text: t("applyUpdate"),
-                onClick: () => window.location.reload(),
-              },
-            },
-            "UpdateManger",
-          );
+          window.location.reload();
         },
       });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return null;
